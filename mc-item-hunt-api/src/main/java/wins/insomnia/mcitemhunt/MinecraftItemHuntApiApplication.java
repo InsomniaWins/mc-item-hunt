120508c0c65435a545bcdc74fb2995ce0af081db
Insomnia_Wins <57158000+InsomniaWins@users.noreply.github.com>
2026-03-16 04:37:09 -0500

    initial commit

---
+++ mc-item-hunt-api/src/main/java/wins/insomnia/mcitemhunt/MinecraftItemHuntApiApplication.java
@@ -0,0 +1,13 @@
+package wins.insomnia.mcitemhunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftItemHuntApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinecraftItemHuntApiApplication.class, args);
+	}
+
+}
---
+++ mc-item-hunt-api/src/test/java/wins/insomnia/mcitemhunt/MinecraftItemHuntApiApplicationTests.java
@@ -0,0 +1,13 @@
+package wins.insomnia.mcitemhunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftItemHuntApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
